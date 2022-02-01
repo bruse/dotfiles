d658431e929656dd6d2b9742205a77c62d564bed
Andreas Bruse <andreas@bruse.se>
2022-02-01 18:01:09 +0100

    Adjust key repeat speed

--- macOS/settings.sh
+++ macOS/settings.sh
@@ -101,8 +101,8 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 5
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
 
 # Set language and text formats
 defaults write NSGlobalDomain AppleLanguages -array "en" "SE"
